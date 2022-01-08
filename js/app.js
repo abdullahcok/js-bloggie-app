9d2840f52a6e51f4813172459a29f880db56f05e
Abdullah Çok <abdullahcok@yandex.com>
2022-01-08 14:42:47 +0300

    sass updated

--- js/app.js
+++ js/app.js
@@ -1,81 +1,125 @@
-const postsContainer = document.getElementById('posts-container');
-const loading = document.querySelector('.loader');
-const filter = document.getElementById('filter');
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endGameEl = document.getElementById('end-game-container');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
 
-let limit = 5;
-let page = 1;
+
+const words = [
+  'sign for', 'i am the danger', 'playmaker', 'independent', 'rome', 'full-stack developer', 'evening lightlamp', 'advance english language', 'daily podcast', 'start-up for future', 'may is coming', 'daily projects make me crazy', 'caligula was right', 'fatih the conquerer', 'willy-hernangomez'
+];
+
+// init word
+let randomWord;
+
+//init score
+let score = 0;
+
+//init time
+let time = 15;
 
 
-// fetch posts from api
-async function getPosts() {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
-  );
+//set difficulty to value in localStorage or slow
+let difficulty = localStorage.getItem('difficulty') !== null ?
+                 localStorage.getItem('difficulty') : 'slow';
 
-  const data = await res.json();
+// set difficulty to select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null ?
+                         localStorage.getItem('difficulty') : 'slow';
 
-  return data;
+//focus on text on start
+text.focus();
+
+// start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+
+// generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
 }
 
-// show posts in dom
-async function showPosts() {
-  const posts = await getPosts();
+// console.log(getRandomWord());
 
-  posts.forEach(post => {
-    const postEl = document.createElement('div');
-    postEl.classList.add('post');
-    postEl.innerHTML = `
-      <div class="number">${post.id}</div>
-      <div class="post-info">
-        <h6 class="post-title">${post.title}</h6>
-        <p class="post-body">${post.body}</p>
-      </div>
-    `;
 
-    postsContainer.appendChild(postEl);
-  });
+//add word to dom
+function addWordToDom() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
 }
 
-// show loader & fetch more posts
-function showLoading() {
-  loading.classList.add('show');
-
-  setTimeout(() => {
-    loading.classList.remove('show');
-
-    setTimeout(() => {
-      page++;
-      showPosts();
-    }, 300);
-  }, 1000);
+// update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
 }
 
-// filter posts by input
-function filterPosts(e) {
-  const term = e.target.value.toUpperCase();
-  const posts = document.querySelectorAll('.post');
 
-  posts.forEach(post => {
-    const title = post.querySelector('.post-title').innerText.toUpperCase();
-    const body = post.querySelector('.post-body').innerText.toUpperCase();
+//update time
+function updateTime() {
+  // console.log(1);
+  time--;// console.log(1);
+  timeEl.innerHTML = time + 's';
 
-    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
-      post.style.display = 'flex';
+  if(time === 0){
+    clearInterval(timeInterval);
+
+    gameOver();
+  }
+}
+
+// game over show on the screen
+function gameOver(){
+  endGameEl.innerHTML = `
+    <h1>Time is over!</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endGameEl.style.display = 'flex';
+}
+
+
+addWordToDom();
+
+// ----------- event listners /////
+
+// typing
+text.addEventListener('input', e => {
+  const insertedText = e.target.value;
+  // console.log(insertedText);
+
+  if(insertedText === randomWord){
+    addWordToDom();
+    updateScore();
+
+    // clear
+    e.target.value = ''
+
+    if (difficulty === 'fast') {
+      time += 2;
+    } else if (difficulty === 'average') {
+      time += 3;
     } else {
-      post.style.display = 'none';
+      time += 5;
     }
-  });
-}
 
-// show initial posts
-showPosts();
-
-window.addEventListener('scroll', () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-  if (scrollTop + clientHeight >= scrollHeight - 3) {
-    showLoading();
+    updateTime();
   }
 });
 
-filter.addEventListener('input', filterPosts);
+
+// settings btn click
+settingsBtn.addEventListener('click', () =>
+    settings.classList.toggle('hide'));
+
+// settings select
+settingsForm.addEventListener('change', e=> {
+  difficulty = e.target.value;
+  //console.log(difficulty);
+  localStorage.setItem('difficulty', difficulty);
+})
